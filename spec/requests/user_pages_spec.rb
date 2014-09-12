939ac2695d9ae6496893ba6845251f8f4af81409
badver <badver@mail.ru>
2014-09-12 15:32:44 +0400

    some fixes

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -1,10 +1,10 @@
 require 'spec_helper'
 
-describe "User pages" do
+describe 'User pages' do
 
   subject { page }
 
-  describe "signup page" do
+  describe 'signup page' do
     before { visit signup_path }
 
     it { should have_content('Sign up') }
